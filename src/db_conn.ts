dce88f9e418e8de0dd552fc47f6d5bee5966c4cd
Robert <robert.s.forristall@gmail.com>
2024-11-13 21:56:54 -0500

    feat: add basic API database connection

---
+++ src/db_conn.ts
@@ -0,0 +1,15 @@
+import { createConnection } from "mysql";
+
+const db = createConnection({
+    host: process.env.DB_URI,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE,
+})
+
+db.connect(err => {
+    if (err) throw err;
+    console.log("API Database connection established successfully")
+})
+
+module.exports = db;
