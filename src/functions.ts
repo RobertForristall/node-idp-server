960e670e971dd1041295aef681d1395b66b71703
Robert <robert.s.forristall@gmail.com>
2024-11-18 21:23:43 -0500

    feat: initial function for checking if a logged in user is an admin for the IDP server

--- src/functions.ts
+++ src/functions.ts
@@ -18,6 +18,23 @@ export function isAuthenticated(
   });
 }
 
+export function isAdmin(
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) {
+  console.log("Is Admin Check Route");
+  req.sessionStore.get(req.sessionID, (err, session) => {
+    if (err) {
+      res.status(403).json(err)
+    } else if (session?.user.application == "IDP" && session?.user.role == "Admin") {
+      next();
+    } else {
+      res.status(403).json("User does not have the authorization to access this route.")
+    }
+  })
+}
+
 export function verifyEmail(email: string) {
   return validator.isEmail(email)
 }
